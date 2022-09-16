ccd615bb1a01d135fd98fcbeae2146b6b57fc53b
Alexandre Vaz de Mello <102325671+alevazdemello@users.noreply.github.com>
2022-09-15 22:33:42 -0300

    Update ex5_2.js

--- Aula2/ex5_2.js
+++ Aula2/ex5_2.js
@@ -1,8 +1,15 @@
-function calcularValor(){
-    var valor = document.getElementById("invalor").value;
-    var tempUso = document.getElementById("intemp").value;
+function calcularValor() {
+  var valor = document.getElementById("invalor").value;
+  var tempUso = document.getElementById("intemp").value;
 
-    document.getElementById("outres").textContent = valor;
+  var pagar = Math.ceil(tempUso / 15) * valor
+  if (valor <= 0) {
+    window.alert('Digite um valor válido.')
+  }
+  if (tempUso <= 0) {
+    window.alert('Digite um valor válido.')
+  }
+  document.getElementById("outres").textContent = `Valor a pagar R$: ${pagar.toFixed(2)}.`;
 }
 
 btpreco.addEventListener("click", calcularValor)
