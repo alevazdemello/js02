d72c170b2f6d2a368a36ec26dca73ce190f48807
Alexandre Vaz de Mello <102325671+alevazdemello@users.noreply.github.com>
2022-09-21 11:31:59 -0300

    Programa Caixa Eletrônico
    (c button p resetar dados)

--- Cap3/ex5_3.js
+++ Cap3/ex5_3.js
@@ -1,42 +1,53 @@
-function exibirNotas() {
-  var inValor = document.getElementById("inValor");
-  var saque = Number(inValor.value);
-  var outNotadeCem = document.getElementById("outNotaCem");
-  var outNotadeCinquenta = document.getElementById("outNotadeCinquenta");
-  var outNotadeDez = document.getElementById("outNotadeDez");
+function calcularNotas() {
+    var inValor = document.getElementById("inValor");
+    var outNotasCem = document.getElementById("outNotasCem");
+    var outNotasCinquenta = document.getElementById("outNotasCinquenta");
+    var outNotasDez = document.getElementById("outNotasDez");
+    var saque = Number(inValor.value); //converte o conteúdo em número
+    document.getElementById("outRes1").textContent = `Valor total solicitado ${saque}.`;
 
-  outNotadeCem.textContent = "";
-  outNotadeCinquenta.textContent = "";
-  outNotadeDez.textContent = "";
+    //verifica se nao preencheu ou Nan
+
+    if(saque == 0 || isNaN(saque)) {
+        alert("Informe o valor do saque corretamente");
+        inValor.focus();
+        return;
+    }
 
 
-  if (saque == 0 || isNaN(saque)) {
-    alert("Informe o valor do saque corretamente");
-    inValor.focus()
-    return;
-  }
+    //verificar se saque não é multiplo de 10:
 
-  if (saque % 10 != 0) {
-    alert("Valor inválido para notas disponíveis (R$10, 50, 100)");
-    inValor.focus();
-    return;
-  }
+    if(saque % 10 != 0) {
+        alert("Valor inválido para notas disponíveis (R$10, 50, 100)");
+        inValor.focus();
+        return;
+    }
 
-  var notasCem = Math.floor(saque / 100);
-  var resto = saque % 100;
-  var notasCinquenta = Math.floor(resto / 50);
-  var resto = saque % 50;
-  var notasDez = Math.floor(resto / 10);
 
-  if (notasCem > 0) {
-    outNotaCem.textContent = "Notas de R$100: " + notasCem;
-  }
-  if (notasCinquenta > 0) {
-    outNotaCinquenta.textContent = "Notas de R$50: " + notasCinquenta;
-  }
-  if (notasDez > 0) {
-    outNotaDez.textContent = "Notas de R$10: " + notasDez;
-  }
+    var notasCem = Math.floor(saque / 100);
+    //cria uma variavel resto para continuar a contabilidade das notas (caso necessario)
+    resto = saque % 100;
+    var notasCinquenta = Math.floor(resto / 50);
+    //caso necessario a variavel resto é alterada
+    resto = resto % 50 
+    var notasDez = Math.floor(resto / 10);
+
+    //exibe as notas apenas se houver:
+    if (notasCem > 0) {
+        outNotasCem.textContent = "Notas de R$100: " + notasCem;
+    }
+    if (notasCinquenta > 0) {
+        outNotasCinquenta.textContent = "Notas de R$50: " + notasCinquenta;
+    }
+    if (notasDez > 0) {
+        outNotasDez.textContent = "Notas de R$10: " + notasDez;
+    }
 }
 
-btExibir.addEventListener("click", exibirNotas);
+function limparCampos() {
+    location.reload();
+    document.getElementById("inValor").focus();
+}
+
+btExibir.addEventListener("click", calcularNotas)
+btLimparDados.addEventListener("click", limparCampos)
