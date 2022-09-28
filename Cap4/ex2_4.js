2028623a0e37eba1a4bed8de902886dcf782d982
Alexandre Vaz de Mello <102325671+alevazdemello@users.noreply.github.com>
2022-09-28 15:55:34 -0300

    Atualização programa contagem regressiva

--- Cap4/ex2_4.js
+++ Cap4/ex2_4.js
@@ -1,12 +1,26 @@
+/*O comando for é particularmente interessante de ser utilizado quando 
+soubermos o número de repetições que devem ocorrer no programa. */ 
+
 function contagem() {
     var inNum = document.getElementById("inNum");
+    var outRes = document.getElementById("outRes");
     var num = Number(inNum.value);
 
-    var resposta = "";
-    for (var i = num; i > 0; i = i - 1) {
-        resposta =  resposta + i + " " ;
+    if (num == 0 || isNaN(num)) {
+        alert("Informe um número válido!");
+        inNum.focus();
+        return;
     }
-    document.getElementById("outRes").textContent = `A contagem decrescente de ${num} é: \n ${resposta}.`;
+
+    var resposta = "Entre " + num + " e 1: ";
+    for (var i = num; i > 1; i = i - 1) {
+        resposta = resposta + i + ", "; //variável local que existe na função.
+    }
+    resposta = resposta + i + ".";
+    outRes.textContent = resposta;
+/* Após o término do laço, ela mantém o valor do último incremento/decremento da repetição.
+    Portanto, ao referenciar o i fora do laço, o conteúdo “1” + “.” é acrescentado à resposta. */
+
 }
 
 function limparDados() {
@@ -14,5 +28,6 @@ function limparDados() {
     document.getElementById("inNum").focus();
 }
 
+var btDecre = document.getElementById("btDecre");
 btDecre.addEventListener("click", contagem);
 btLimp.addEventListener("click", limparDados);
